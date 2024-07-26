cef68cc9744c31fe56b5b338c33befb949d62f3b
Kerry Kidd <kerry@raspikidd.com>
2024-07-26 14:46:46 +0100

    Updated Classes Book a call link

--- pages/Classes.vue
+++ pages/Classes.vue
@@ -90,7 +90,7 @@
                         class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">
                         Book Classes
                     </NuxtLink>
-                    <a href="https://calendly.com/raspikidd/30-min-discovery-call" 
+                    <a href="https://app.simplymeet.me/raspikidd/discovery-call" 
                         class="text-sm font-semibold leading-6 text-grey-900"> 
                         Book a Call 
                         <span aria-hidden="true">
